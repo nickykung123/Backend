f989418defd4977ca92e0163a4905e3803a9f075
nickykung123 <150765575+nickykung123@users.noreply.github.com>
2024-10-29 13:33:23 +0700

    asdasdas

--- app/(website)/home/page.tsx
+++ app/(website)/home/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex w-full items-center justify-center text-4xl mt-4 border border-white h-20">
-      หน้าหลัก
+      หน้าหลักijetijro
     </div>
   );
 }
